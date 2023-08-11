08bff838a791e2c0ed37f34844445033e4ab778b
aaphan <atp2323@utexas.edu>
2023-08-11 13:21:08 -0500

    1:21 PM timestamp, final version v1

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import glob
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("OdderTech Game Corner")
-rate = 60
+rate = 15
 ICON_WIDTH, ICON_HEIGHT = 130, 90
 ICON_XPOS_L, ICON_XPOS_M, ICON_XPOS_R = 175, 385, 595
 ICON_YPOS = 170
@@ -50,9 +50,24 @@ bg = pygame.transform.scale(
 # likely implement this by reducing the frame rate so that it stops updaitng
 
 
-def draw_window(leftSprite, midSprite, rightSprite):
+# draw_window functions for each sprite
+def draw_bg():
     WIN.fill((255, 255, 255))
     WIN.blit(bg, (0, 0))
+    pygame.display.update()
+
+
+def draw_window1(leftSprite):
+    IMAGE_LEFT = random.choice(
+        [
+            Veilstone_7,
+            Veilstone_Berries,
+            Veilstone_Galactic,
+            Veilstone_Lightning,
+            Veilstone_Moon,
+            Veilstone_Replay,
+        ]
+    )
     WIN.blit(
         random.choice(
             [
@@ -66,22 +81,67 @@ def draw_window(leftSprite, midSprite, rightSprite):
         ),
         (leftSprite.x, leftSprite.y),
     )
-    WIN.blit(Veilstone_7, (midSprite.x, midSprite.y))
-    WIN.blit(Veilstone_7, (rightSprite.x, rightSprite.y))
     pygame.display.update()
+    return IMAGE_LEFT
+
+
+def draw_window2(midSprite):
+    IMAGE_MID = random.choice(
+        [
+            Veilstone_7,
+            Veilstone_Berries,
+            Veilstone_Galactic,
+            Veilstone_Lightning,
+            Veilstone_Moon,
+            Veilstone_Replay,
+        ]
+    )
+    WIN.blit(
+        random.choice(
+            [
+                Veilstone_7,
+                Veilstone_Berries,
+                Veilstone_Galactic,
+                Veilstone_Lightning,
+                Veilstone_Moon,
+                Veilstone_Replay,
+            ]
+        ),
+        (midSprite.x, midSprite.y),
+    )
+    pygame.display.update()
+    return IMAGE_MID
+
+
+def draw_window3(rightSprite):
+    IMAGE_RIGHT = random.choice(
+        [
+            Veilstone_7,
+            Veilstone_Berries,
+            Veilstone_Galactic,
+            Veilstone_Lightning,
+            Veilstone_Moon,
+            Veilstone_Replay,
+        ]
+    )
+    WIN.blit(
+        random.choice(
+            [
+                Veilstone_7,
+                Veilstone_Berries,
+                Veilstone_Galactic,
+                Veilstone_Lightning,
+                Veilstone_Moon,
+                Veilstone_Replay,
+            ]
+        ),
+        (rightSprite.x, rightSprite.y),
+    )
+    pygame.display.update()
+    return IMAGE_RIGHT
 
 
 # conditions
-
-
-# movement
-def icon_movement(keys_pressed, leftSprite, midSprite, rightSprite):
-    """if keys_pressed[pygame.K_f]:
-    leftSprite.y += 1"""
-    if keys_pressed[pygame.K_g]:
-        midSprite.y += 1
-    if keys_pressed[pygame.K_h]:
-        rightSprite.y += 1
 
 
 def main():
@@ -90,26 +150,65 @@ def main():
     rightSprite = pygame.Rect(ICON_XPOS_R, ICON_YPOS, ICON_WIDTH, ICON_HEIGHT)
     run = True
     clock = pygame.time.Clock()
-
+    draw_bg()
     while run:
         clock.tick(rate)
         keys_pressed = pygame.key.get_pressed()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run = False
+                pygame.quit()
                 print("Hello")
             if keys_pressed[pygame.K_f]:
                 run = False
                 print("Correct")
 
-        icon_movement(keys_pressed, leftSprite, midSprite, rightSprite)
-        draw_window(leftSprite, midSprite, rightSprite)
+        # icon_movement(keys_pressed, leftSprite, midSprite, rightSprite)
+        draw_bg()
+        sprite_left = draw_window1(leftSprite)
+        draw_window2(midSprite)
+        draw_window3(rightSprite)
+
+    # first sprite is stopped
+    stop_con1 = True
+    while stop_con1:
+        clock.tick(rate)
+        keys_pressed = pygame.key.get_pressed()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                print("Hello")
+            if keys_pressed[pygame.K_g]:
+                stop_con1 = False
+                print("Correct2")
+        draw_bg()
+        WIN.blit(sprite_left, (leftSprite.x, leftSprite.y))
+        sprite_mid = draw_window2(midSprite)
+        draw_window3(rightSprite)
+    stop_con2 = True
+    while stop_con2:
+        clock.tick(rate)
+        keys_pressed = pygame.key.get_pressed()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                print("Hello")
+            if keys_pressed[pygame.K_h]:
+                stop_con2 = False
+                print("Correct3")
+        draw_bg()
+        WIN.blit(sprite_left, (leftSprite.x, leftSprite.y))
+        WIN.blit(sprite_mid, (midSprite.x, midSprite.y))
+        sprite_right = draw_window3(rightSprite)
     idle = True
     while idle:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 idle = False
                 print("Hello")
+        WIN.blit(sprite_right, (rightSprite.x, rightSprite.y))
+        keys_pressed = pygame.key.get_pressed()
+        if keys_pressed[pygame.K_SPACE]:
+            main()
 
     pygame.quit()
 
